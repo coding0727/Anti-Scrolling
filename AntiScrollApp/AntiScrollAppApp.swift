fcf60a963ee70ec6e5287493d853b1f63b409515
Phoebe Zhong <meetfuntravel@gmail.com>
2026-08-12 22:00:35 -0700

    Initial Commit

---
+++ AntiScrollApp/AntiScrollAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AntiScrollAppApp.swift
+//  AntiScrollApp
+//
+//  Created by Phoebe Zhong on 8/12/26.
+//
+
+import SwiftUI
+
+@main
+struct AntiScrollAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
